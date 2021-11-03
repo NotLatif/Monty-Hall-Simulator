84d0cc42683e9f4151a52b34514dcc6e8054bf65
Latif <latif.terziu03@gmail.com>
2021-11-03 20:03:20 +0100

    Added comments

--- caller.py
+++ caller.py
@@ -3,29 +3,31 @@ import sys
 from random import randrange
 import time
 
+filename = "GameShowCallable"
+
 def run(times, switch = 2):
 	if (switch == 2):
 		for x in range(times):
 			door = randrange(3) +1
 			switch = randrange(2)
-			os.system('python GameShowCallable.py %s %s' %(door, switch))
+			os.system('python %s.py %s %s' %(filename, door, switch))
 	else:
 		for x in range(times):
 			door = randrange(3) +1
-			os.system('python GameShowCallable.py %s %s' %(door, switch))
+			os.system('python %s.py %s %s' %(filename, door, switch))
 
 
 start = time.time()
 
-try:
+try: #first argument defines how many times the loop iterates
     times = int(sys.argv[1])
-except IndexError:
+except IndexError: #no arguments, the loop iterates one time
     times = 1
 
-try:
+try: #second argument forces switching (0: no, 1: yes)
     switch = int(sys.argv[2])
     run(times, switch)
-except IndexError:
+except IndexError: #no argument, the switching is random
     run(times)
 
 
